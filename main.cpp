3a42503f567793fbf7f56f6c0e470253aeebaca2
Andrea Safadi <117497344+safadiandrea@users.noreply.github.com>
2025-04-16 18:27:17 -0400

    function-update_main.cpp
    feat: added new remove and search functionality
    
    - added new stringRemove() function to implement removal by author and name (in addition to previous remove() function which removes book by ID number) 
    - added new search() function to include search by author and name (in addition to previous find() function which searches book by ID number)
    - code cleanup and reformatting 
    - additional comments added explaining logic

--- main.cpp
+++ main.cpp
@@ -6,165 +6,209 @@ using namespace std;
 
 
 /*
+
+Group Members: Vesper Nyugen, Tiffany Rodriguez, Silver, Andrea Safadi
+
 Our main focus is implementation of Hash Map class
 We will need functions to sort based on Name, Author, and ID
 
 Linked List (<list>) is used since it's more efficient to add, remove at front
 or end for managing. Additionally, it's also a part of the concept for this
 semester.
+
 */
 
-//Main structure of a book
-struct Book
-{
+// Main structure of a book
+struct Book {
     string name;
     string author;
     int id;
 };
 
-//Hash Map owns implementation
-class HashMap
-{
+// Hash Map implementation as a class with functions
+class HashMap {
+
 private:
+
     int size;
-    list<Book>* map;
+    list<Book> *map;
 
     //Main hash function
-    int hashFunction(int key)
-    {
+    int hashFunction(int key) {
         return key % size;
     }
-    
+
 public:
-    //Constructor
-    HashMap(int a)
-    {
+
+    // Constructor
+    HashMap(int a) {
         size = a;
-        map = new list<Book>[size];
+        map = new list<Book>[size]; // dynamic memory allocation of linked list array
     }
 
-    //Add function
-    void add(Book book)
-    {
-      int index = hashFunction(book.id);
-      map[index].push_back(book);
+    // Add a book
+    void add(Book book) {
+        int index = hashFunction(book.id); // maps the corresponding book id to the hash table index
+        map[index].push_back(book); // adds book to hashtable at the index
+        cout << "Book was added successfully" << endl;
     }
 
-    //Remove function
-    void remove(int id)
-    {
-        int index = hashFunction(id);
-        for (list<Book>::iterator i = map[index].begin(); i != map[index].end(); i++)
-        {
-         if (i->id == id)
-           {
-           map[index].erase(i);
-           cout << "Book removed with ID " << id << " ." << endl;
-           return;
-           }
-        }
-        cout << "Book doesn't exist within the library." << endl;
-    }
-
-    //Sort function
-    void sort(string criteria)
-    {
-        Book books[9999];
-        int counter = 0;;
-
-        for (int i = 0; i < size; i++)
-          {
-          for (list<Book>::iterator it = map[i].begin(); it != map[i].end(); it++)
+    // Remove book by ID only
+    void remove(int id) {
+        int index = hashFunction(id); // searches for book at index
+        for (list<Book>::iterator i = map[index].begin(); i != map[index].end(); i++) {
+            if (i->id == id) // if the id within the index matches the given book id
             {
-            if (counter < 9999)
-              {
-              books[counter++] = *it;
-              }
+                map[index].erase(i); // removes it from the list
+
+                cout << "Book removed with ID " << id << " ." << endl;
+                return;
             }
-          }
-          //Bubble sort
-          for (int i = 0; i < counter - 1; i++)
-            {
-            for (int j = 0; j < counter - i - 1; j++)
-              {
-              bool swapCondition = false;
-              if (criteria == "name" && books[j].name > books[j + 1].name)
-              {
-                    swapCondition = true;
-              }
-              else if (criteria == "author" && books[j].author > books[j + 1].author)
-              {
-                    swapCondition = true;
-              }
-              else if (criteria == "id" && books[j].id > books[j + 1].id)
-              {
-                    swapCondition = true;
-              }
-              if (swapCondition == true)
-                {
-                Book temp = books[j];
-                books[j] = books[j + 1];
-                books[j + 1] = temp;
+        }
+        cout << "Book doesn't exist within the library." << endl; // else the book wasn't found
+    }
+
+    // Removes a book by name or by author
+    void stringRemove(string name, string type) {
+        for (int i = 0; i < size; i++) {
+            // traverses hashtable
+
+            for (auto it = map[i].begin(); it != map[i].end();) {
+                // iterates through linked list at current index
+
+                // if name or author given matches any found in linked list at that index, gets removed
+                if ((name == "name" && it->name == type) || (name == "author" && it->author == type)) {
+                    cout << it->name << " by " << it->author << " was removed";
+                    it = map[i].erase(it); // removes book
+                } else {
+                    ++it; // moves iterator to next book
                 }
             }
-          }
-          //Display after sort
-          for (int i = 0; i < counter; i++)
-            {
-              cout << "Name: " << books[i].name << "\nAuthor: " << books[i].author << "\nID: " << books[i].id << "\n" << endl;
-            }
+        }
     }
 
-    //Find function
-    void find(int id)
-    {
-        int index = hashFunction(id);
-        for (list<Book>::iterator i = map[index].begin(); i != map[index].end(); ++i)
-        {
-            if (i->id == id)
-          {
-                cout << "Book found within library:\n";
+    // Sorts books through bubble sort
+    void sort(string criteria) {
+        Book books[9999]; // book array
+        int counter = 0; // tracks number of books
+
+        for (int i = 0; i < size; i++) {
+            // traverses hash table
+
+            for (list<Book>::iterator it = map[i].begin(); it != map[i].end(); it++) {
+                // iterates through current linked list at i
+                if (counter < 9999) {
+                    // if book number is less than size of array
+                    books[counter++] = *it; // copy book from hash table into fixed array books[]
+                } // increment counter ^
+            }
+        }
+
+        // Bubble sort implementation
+        for (int i = 0; i < counter - 1; i++) {
+            // loops while 'i' is less than the amount of books copied - 1 (last book is sorted)
+
+            for (int j = 0; j < counter - i - 1; j++) {
+                // compares current index with the next index
+
+                bool swapCondition = false; // checks if books should swap position within array
+
+                if (criteria == "name" && books[j].name > books[j + 1].name) {
+                    // compares name [j] with [j+1]
+                    swapCondition = true; // swaps to alphabetical order of book name
+                } else if (criteria == "author" && books[j].author > books[j + 1].author) {
+                    // compares author [j] with [j+1]
+                    swapCondition = true; // swaps to alphabetical order of book author
+                } else if (criteria == "id" && books[j].id > books[j + 1].id) {
+                    // compares id of [j] with [j+1]
+                    swapCondition = true; // swaps based on numerical order of id number
+                }
+
+                if (swapCondition == true) {
+                    // swaps books
+                    Book temp = books[j];
+                    books[j] = books[j + 1];
+                    books[j + 1] = temp;
+                }
+            }
+        }
+
+        // Displays sorted books after sort function resolution
+        for (int i = 0; i < counter; i++) {
+            cout << "Name: " << books[i].name << "\nAuthor: " << books[i].author << "\nID: " << books[i].id << "\n" <<
+                    endl;
+        }
+    }
+
+    // Find book by ID only
+    void find(int id) {
+        int index = hashFunction(id); // maps given id to index on the hash table
+        for (list<Book>::iterator i = map[index].begin(); i != map[index].end(); ++i) {
+            // traverses linked list at given index
+
+            if (i->id == id) {
+                // if the id in the linked list matches given id
+                cout << "Book found within library:\n"; // indicate book was found
                 cout << "Name: " << i->name << "\nAuthor: " << i->author << "\nID: " << i->id << endl;
                 return;
-          }
+            }
         }
-        cout << "Book not found within library.\n";
+        cout << "Book not found within library.\n"; // else book was not found
     }
 
-    //Display function
-    void display()
-    {
-      cout << "\nAll books in the library system:\n";
-        for (int i = 0; i < size; ++i)
-      {
-            for (list<Book>::iterator it = map[i].begin(); it != map[i].end(); ++it)
-              {
+    // search for a book by name or author
+    void search(string name, string type) {
+        for (int i = 0; i < size; i++) {
+            // traverses hash table
+
+            for (list<Book>::iterator it = map[i].begin(); it != map[i].end(); it++) {
+                // traverse linked list within current index
+                // if search type + given name or author matches data found in hash table index
+                if ((name == "name" && it->name == type) || (name == "author" && it->author == type)) {
+                    cout << "Book found within library:\n"; // indicate book was found
+                    cout << "Name: " << it->name << "\nAuthor: " << it->author << "\nID: " << it->id << endl;
+                    return;
+                }
+            }
+        }
+        cout << "Book not found within library.\n"; // else book was not found
+    }
+
+    // Displays all books within the library
+    void display() {
+        cout << "\nAll books in the library system:\n" << endl;
+        for (int i = 0; i < size; ++i) {
+            // traverse hash table
+            for (list<Book>::iterator it = map[i].begin(); it != map[i].end(); ++it) {
+                // traverse linked list at [i]
                 cout << "Name: " << it->name << "\nAuthor: " << it->author << "\nID: " << it->id << "\n" << endl;
-              }
-      }
+            } // displays books at each index ^
+        }
     }
 
-    //Fill 3 books in library
-    void fill()
-    {
-      Book book1 = {"The Great Gatsby", "F. Scott Fitzgerald", 45565};
-      Book book2 = {"1984", "George Orwell", 31834};
-      Book book3 = {"To Kill a Mockingbird", "Harper Lee", 91045};
-      add(book1);
-      add(book2);
-      add(book3);
+    // Starts program with 3 books in the library
+    void fill() {
+        Book book1 = {"The Great Gatsby", "F. Scott Fitzgerald", 45565};
+        Book book2 = {"1984", "George Orwell", 31834};
+        Book book3 = {"To Kill a Mockingbird", "Harper Lee", 91045};
+        add(book1);
+        add(book2);
+        add(book3);
     }
 };
 
-//Main
-int main()
-{
-  HashMap HashMap(20);
-  HashMap.fill();     //Fill 3 books to demonstrate, can be disable
-  int choice;
-  //Main menu
-    while (true)
-    {
+
+// main
+int main() {
+
+//-------------------------------------------------------------------------------------------------
+
+    HashMap HashMap(20);
+    HashMap.fill(); // Fills library with 3 books to demonstrate, can be disabled
+    int choice;
+
+    // Main menu input loop
+    while (true) {
         cout << "\nMenu:\n";
         cout << "1) Add book\n";
         cout << "2) Remove book\n";
@@ -175,8 +219,10 @@ int main()
         cout << "Enter choice: ";
         cin >> choice;
 
-        if (choice == 1)
-        {
+//-------------------------------------------------------------------------------------------------
+
+        if (choice == 1) {
+            // add a book
             Book book;
             cout << "Enter book name: ";
             cin.ignore();
@@ -186,41 +232,109 @@ int main()
             cout << "Enter book ID: ";
             cin >> book.id;
             HashMap.add(book);
-        }
-        else if (choice == 2)
-        {
-            int id;
-            cout << "Enter book ID to remove: ";
-            cin >> id;
-            HashMap.remove(id);
-        }
-        else if (choice == 3)
-        {
+//-------------------------------------------------------------------------------------------------
+
+        } else if (choice == 2) {
+            // remove a book
+            // initialize variables
+            Book book;
+            int option;
+            int bookId;
+            string bookName;
+            string bookAuthor;
+
+            cout << "Remove book by (Please choose corresponding number option): " << std::endl;
+            cout << "[1] Book Name " << endl;
+            cout << "[2] Book Author " << endl;
+            cout << "[3] Book ID " << endl;
+            cout << "[4] Back to Main Menu " << endl;
+            cin >> option;
+            cin.ignore();
+
+            if (option == 1) {
+                // remove by name
+                cout << "Enter the name of the book you wish to remove: " << endl;
+                cin >> bookName;
+                HashMap.stringRemove("name", bookName);
+            } else if (option == 2) {
+                // remove by author
+                cout << "Enter the author of the book you wish to remove: " << endl;
+                getline(cin, bookAuthor);
+                HashMap.stringRemove("author", bookAuthor);
+            } else if (option == 3) {
+                // remove by ID
+                cout << "Enter the ID of the book you wish to remove: " << endl;
+                cin >> bookId;
+                HashMap.remove(bookId);
+            } else if (option == 4) {
+                // go back to main menu
+                continue;
+            }
+//------------------------------------------------------------------------------------------------
+
+        } else if (choice == 3) {
+            // Sort books (bubble sort)
             string criteria;
             cout << "Enter sort criteria (name, author, id): ";
             cin >> criteria;
             HashMap.sort(criteria);
-        }
-        else if (choice == 4)
-        {
-            int id;
-            cout << "Enter book ID to find: ";
-            cin >> id;
-            HashMap.find(id);
-        }
-        else if (choice == 5)
-        {
+//-------------------------------------------------------------------------------------------------
+
+        } else if (choice == 4) {
+            // search for a book
+            // initialize variables
+            Book book;
+            int option2;
+            int bookId;
+            string bookName;
+            string bookAuthor;
+
+            cout << "Choose search method by typing in the corresponding number choice: " << std::endl;
+            cout << "[1] Book Name " << endl;
+            cout << "[2] Book Author " << endl;
+            cout << "[3] Book ID " << endl;
+            cout << "[4] Back to Main Menu " << endl;
+            cin >> option2;
+            cin.ignore();
+
+            // option result branches
+            if (option2 == 1) {
+                // search by book name
+                cout << "Enter book name: " << endl;
+                cin >> bookName;
+                HashMap.search("name", bookName);
+            } else if (option2 == 2) {
+                // search by book author
+                cout << "Enter book author: " << endl;
+                getline(cin, bookAuthor);
+                HashMap.search("author", bookAuthor);
+            } else if (option2 == 3) {
+                // search by book ID
+                cout << "Enter book ID: " << endl;
+                cin >> bookId;
+                HashMap.find(bookId);
+            } else if (option2 == 4) {
+                // returns to main menu
+                continue;
+            } else {
+                // no valid option chosen
+                cout << "Invalid choice." << endl;
+            }
+//------------------------------------------------------------------------------------------------
+
+        } else if (choice == 5) {
+            // displays library
             HashMap.display();
-        }
-        else if (choice == 6)
-        {
-            break;
-        }
-        else
-        {
+//-------------------------------------------------------------------------------------------------
+
+        } else if (choice == 6) {
+            // exits program
+            return 0;
+//-------------------------------------------------------------------------------------------------
+
+        } else {
+            // invalid input
             cout << "Choose a valid option.\n";
         }
     }
-
-    return 0;
 }
